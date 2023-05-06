cbe3688939828c2e3e893cc0caa68ddbefa9bbe5
Vedavyasa Rao <33799212+VedavyasaRao@users.noreply.github.com>
2023-05-07 01:42:16 +0530

    updates

--- Regular Expressions/examples.cpp
+++ Regular Expressions/examples.cpp
@@ -11,12 +11,21 @@ auto reg_replacement = ""s;
 
 void write_header()
 {
-    cout  << "****************"  << endl;
     cout << "Pattern:" << reg_pattern << endl;
     cout << "Input:" << reg_input << endl;
     if (!reg_replacement.empty())
         cout << "Replacement:" << reg_replacement << endl;
-    cout  << "****************"  << endl;
+}
+
+void print_submatches(const boost::smatch m)
+{
+    cout << "Sub matches:" << endl;
+    for (auto i=1u; i<m.size(); ++i)
+    {
+        cout  << m[i]<< endl;
+    }    
+    cout << endl;
+        
 }
 
 void match()
@@ -25,34 +34,32 @@ void match()
     
     boost::smatch m;
     auto b = boost::regex_match ( reg_input, m, reg_pattern);
-    cout << boolalpha << "match:" << b << endl;
-    if (!b)
-    {
-        cout << endl;
-        return;
-    }   
-    
-    for (auto i=0u; i<m.size(); ++i)
-    {
-        cout  << "****************"  <<endl;
-        cout  << m[i]<< endl;
-        cout << "****************"  <<endl;
-      }    
+    cout << boolalpha << "match? " << boolalpha << b << endl;
 }
 
 void replace()
 {
     write_header();
     //Note $0 means full match ; $1 means sub match
-    std::cout << boost::regex_replace(reg_input, reg_pattern, reg_replacement) << '\n';
+    std::cout << "result:" << boost::regex_replace(reg_input, reg_pattern, reg_replacement) <<  endl;;
 }
 
-void search()
+void searchwithsmatch()
+{
+    write_header();
+    boost::smatch reg_matches;
+    if (boost::regex_search(reg_input, reg_matches, reg_pattern))
+    {
+        print_submatches(reg_matches);
+    }
+}
+
+void searchwithtokenizer(int matchsel)
 {
     write_header();
     boost::regex_token_iterator<std::string::iterator> it;
-    //Note: 0 means full match; 1 means sub match(es)
-    it = {reg_input.begin(), reg_input.end(), reg_pattern, {0,1}};
+    //Note: 0 means full match; 1 means sub match(es); -1 means no matches
+    it = {reg_input.begin(), reg_input.end(), reg_pattern, matchsel};
     boost::regex_token_iterator<std::string::iterator> theend;
     if (it == theend)
     {
@@ -60,56 +67,80 @@ void search()
         return;
     }
 
+    cout << "Sub matches:" << endl;
     while (it != theend)
     {
-        cout  << "****************"  << endl;
         cout  << *it << endl;
-        cout << "****************"  << endl;
         ++it;
     }
 }
 
+
 int main()
 {
-    cout << "Match input postive testcase - match SSN" << endl;
+    cout << "match: positive testcase - match SSN" << endl;
     reg_pattern = boost::regex(R"(\d{3}-\d{2}-\d{4})");   
     reg_input = R"(123-45-6789)";    
     match();
     cout << endl;
 
-    cout << "Match input  negative testcase - match Phone number" << endl;
+    cout << "match: negative testcase - match Phone number" << endl;
     reg_pattern = boost::regex(R"(\d{3}-\d{3}-\d{4})");   
     reg_input = R"(123-45-6789)";    
     match();
     cout << endl;
 
-    cout << "Extract from input postive testcase - find adjacent duplicate words" << endl;
-    reg_pattern = boost::regex(R"((\b\w+\b)\s+\1)");   
-    reg_input = R"(state of of  the the art)";    
-    search();
+    cout << "search: positive testcase - extract area code" << endl;
+    cout << "using non recursive smatch" << endl;
+    reg_pattern = boost::regex(R"((\d{3})-\d{3}-\d{4})");   
+    reg_input = R"(408-206-2335)";    
+    //searchwithsmatch();
+    searchwithtokenizer(1);
     cout << endl;
 
-    cout << "Extract from input negative testcase - extract area code" << endl;
+    cout << "search: negative testcase - extract area code" << endl;
+    cout << "using non recursive smatch" << endl;
     reg_pattern = boost::regex(R"((\d{3})-\d{3}-\d{4})");   
     reg_input = R"(123-45-6789)";    
-    search();
+    searchwithsmatch();
+    cout << endl;
+
+    cout << "search: find recurring duplicate words" << endl;
+    cout << "using recursive tokenizer(1)" << endl;
+    reg_pattern = boost::regex(R"((\b\w+\b)\s+\1)");   
+    reg_input = R"(state of of the the art)";    
+    searchwithtokenizer(1);
+    cout << endl;
+
+    cout << "search: find recurring non duplicate words" << endl;
+    cout << "using recursive tokenizer(-1)" << endl;
+    reg_pattern = boost::regex(R"((\b\w+\b)\s+\1)");   
+    reg_input = R"(state of of  the the art)";    
+    searchwithtokenizer(-1);
     cout << endl;
     
-    cout << "Replace text testcase - full replacement with new string" << endl;
+    cout << "replace: full replacement with new string" << endl;
     reg_input = R"(Hello, World!)";    
     reg_pattern = boost::regex("World");   
     reg_replacement = "Khri$ha";
     replace();
     cout << endl;
     
-    cout << "Replace text testcase - full replacement with sub match" << endl;
+    cout << "replace: full replacement with sub match" << endl;
     reg_pattern = boost::regex(R"((\b\w+\b)\s+\1)");   
     reg_input = R"(state of of  the the art)";    
     reg_replacement = "$1";
     replace();
     cout << endl;
 
+    cout << "replace: internationalization with sweden locale" << endl;
+    reg_input = "khri$ha är bättre";
+    reg_pattern.imbue(locale{"sv_SE.UTF-8"});
+    reg_pattern = "bättre";
+    reg_replacement = "bäst😂";
+    replace();
+    cout << endl;
+
     
     return 0;
 }
-
