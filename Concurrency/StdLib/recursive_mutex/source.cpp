b573cf0a003c82d568ae5980091d5b847f05b94a
Vedavyasa Rao <33799212+VedavyasaRao@users.noreply.github.com>
2023-06-15 11:01:12 +0530

    updates

--- Concurrency/StdLib/recursive_mutex/source.cpp
+++ Concurrency/StdLib/recursive_mutex/source.cpp
@@ -8,10 +8,10 @@ using namespace chrono;
 
 recursive_mutex test_mutex;
 
-void create_table()
+void create_table_withoutdata()
 {
 	test_mutex.lock();
-	cout << "creating table" << endl;
+	cout << "creating table without data" << endl;
 	test_mutex.unlock();
 }
 
@@ -22,10 +22,10 @@ void insert_data()
 	test_mutex.unlock();
 }
 
-void create_table_and_insert_data()
+void create_table_withdata()
 {
 	test_mutex.lock();
-	cout << "creating table" << endl;
+	cout << "creating table with data" << endl;
 	insert_data();
 	test_mutex.unlock();
 }
@@ -33,10 +33,10 @@ void create_table_and_insert_data()
 int main()
 {
 	cout << "create table A" << endl;
-	create_table();
+	create_table_withoutdata();
 	cout << "insert data to table A" << endl;
 	insert_data();
 	cout << "create table B and insert data" << endl;
-	create_table_and_insert_data();
+	create_table_withdata();
 
 }
