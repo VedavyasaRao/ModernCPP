c83394ff235bb83384952a010df00b6a0943ec33
Vedavyasa Rao <33799212+VedavyasaRao@users.noreply.github.com>
2023-07-09 02:19:14 +0530

    update

--- Language Features/list - initialization/container/initialize/source.cpp
+++ Language Features/list - initialization/container/initialize/source.cpp
@@ -9,6 +9,6 @@ int main()
 {
     vector<int>  intvec {1,2,3,4,5};
     vector<int>  intvec2 {1,2.0,3,4,5};
-    list<int>  lst {1,2,3,4,5};
+    list<string>  strvec {"mon","tue","wed","thu","fri","sat","sun"};
     map<string,int> strintmap {{"a",97},{"b",98},{"c",99}};
 }
