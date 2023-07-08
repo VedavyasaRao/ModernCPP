dce343a8d7ae6ecadccb575b58c16defdf429bfd
Vedavyasa Rao <33799212+VedavyasaRao@users.noreply.github.com>
2023-07-09 00:13:41 +0530

    update

--- LangFeatures/list - initialization/container/initialize/source.cpp
+++ LangFeatures/list - initialization/container/initialize/source.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() 
 {
     vector<int>  intvec {1,2,3,4,5};
-    vector<int>  intvec2 {1,2,3,4,5};
+    vector<int>  intvec2 {1,2.0,3,4,5};
     list<int>  lst {1,2,3,4,5};
     map<string,int> strintmap {{"a",97},{"b",98},{"c",99}};
 }
