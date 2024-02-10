3d9016ccda0d1a1084cd369d36596935f3747b56
Vedavyasa Rao <33799212+VedavyasaRao@users.noreply.github.com>
2024-02-10 13:02:23 +0530

    update

--- Language Features/varidiac template/pack expansion/print/source.cpp
+++ Language Features/varidiac template/pack expansion/print/source.cpp
@@ -5,86 +5,49 @@
 #include <iomanip>
 #include <sstream>
 #include <ctime>
+#include <tuple>
 using namespace std;
 
-void handlemanipulator(ostream& os, const string& token)
+template <size_t i, typename ...Args>
+struct HandleManipulator
 {
-	if (token == "endl")
-		os << endl;
-	else if (token == "showbase")
-		os << showbase;
-	else if (token == "boolalpha")
-		os << boolalpha;
-	else if (token == "noboolalpha")
-		os << noboolalpha;
-	else if (token == "showpos")
-		os << showpos;
-	else if (token == "noshowpos")
-		os << noshowpos;
-	else if (token == "uppercase")
-		os << uppercase;
-	else if (token == "nouppercase")
-		os << nouppercase;
-	else if (token == "oct")
-		os << oct;
-	else if (token == "dec")
-		os << dec;
-	else if (token == "hex")
-		os << hex;
-	else if (token == "noshowpos")
-		os << noshowpos;
-	else if (token == "showpoint")
-		os << showpoint;
-	else if (token == "fixed")
-		os << fixed;
-	else if (token == "scientific")
-		os << scientific;
-	else if (token == "hexfloat")
-		os << hexfloat;
-	else if (token == "defaultfloat")
-		os << defaultfloat;
-	else if (token == "left")
-		os << left;
-	else if (token == "right")
-		os << right;
-	else if (token == "internal")
-		os << internal;
-	else if (token.find("setprecision") != string::npos)
-		os.precision(stoi(token.substr(13, token.find(')') - 13)));
-	else if (token.find("setw") != string::npos)
-		os.width(stoi(token.substr(5, token.find(')') - 5)));
-	else if (token.find("put_money ") != string::npos)
-		os.width(stoi(token.substr(10, token.find(')') - 10)));
-	else if (token.find("setfill") != string::npos)
-		os.fill(token.substr(token.find('\''))[1]);
-	else
-		os << token;
+    void doit(ostream &os, const tuple<Args...>& tup)
+    {
+        HandleManipulator<i-1,  Args...>{}.doit(os,tup);
+        os << get<i>(tup);
+    }
+};
+
+template <typename ...Args>
+struct HandleManipulator<0, Args...>
+{
+    void doit(ostream &os, const tuple<Args...>& tup)
+    {
+         os << get<0>(tup);
+    }
+};
+
+template<typename ...Args>
+ostream& operator<< (ostream& os, const tuple<Args...> &tup)
+{
+    constexpr int n=sizeof...(Args);
+    HandleManipulator<n-1,Args...>().doit(os,tup);
+    return os;
 }
 
-void handleformat(ostream& os, const char * format)
+template<typename U, typename V>
+ostream& operator<< (ostream& os, const pair<U,V> &p)
 {
-	string formats(format);
-	size_t p = 0, p2 = 0;
-	while (p2 != string::npos)
-	{
-		p2 = formats.find(',', p);
-		auto token = formats.substr(p, p2 - p);
-		handlemanipulator(os, token);
-		p = p2 + 1;
-	}
+    os << p.second;
+    os << p.first;
+    return os;
 }
 
 template<class T>
 void process(ostream& os, T t)
 {
 	os << t;
-}
 
-template<class T>
-void process(ostream& os, const pair<T, char const *>& t)
-{
-	handleformat(os, t.second);
-	os << t.first;
 }
 
 void smartprint(ostream&) {}
@@ -92,7 +55,7 @@ void smartprint(ostream&) {}
 template<typename T, typename... Ts>
 void smartprint(ostream& os, T t, Ts... ts)
 {
-	process(os, t);
+ 	process(os, t);
 	// “ts” is its pattern
 	// “ts...” is  pack expansion
 	smartprint(os, ts...);
@@ -102,26 +65,26 @@ int main()
 {
     time_t t = std::time(nullptr);
     tm tm = *localtime(&t);
+    bool b = true;
     long m = 123456;
     double h = 1.56795;
     const char * ssn ="123";
  
     cout.imbue(locale("en_US.UTF-8"));
 	cout << R"z(	smartprint(cout, 
-        "Citizen\t\t\t:", make_pair(true, "boolalpha"), 
-        "\nheight(meters)\t\t:", make_pair(h, "fixed,setprecision(3)"),
-        "\nincome\t\t\t:", make_pair(put_money(m),  "showbase"),
-        "\nSocial Security number\t:", make_pair(ssn, "left,setw(8),setfill('#'),showbase"),
+        "Citizen\t\t\t:", make_pair(b, make_tuple(boolalpha)), 
+        "\nheight(meters)\t\t:", make_pair(h, make_tuple(fixed,setprecision(3))),
+        "\nincome\t\t\t:", make_pair(put_money(m),  make_tuple(showbase)),
+        "\nSocial Security number\t:", make_pair(ssn, make_tuple(left,setw(8),setfill('#'),showbase)),
         "\ntime stamp\t\t:", put_time(&tm, "%c %Z"),
         "\n"))z" << endl << endl;
 
 	smartprint(cout, 
-        "Citizen\t\t\t:", make_pair(true, "boolalpha"), 
-        "\nheight(meters)\t\t:", make_pair(h, "fixed,setprecision(3)"),
-        "\nincome\t\t\t:", make_pair(put_money(m),  "showbase"),
-        "\nSocial Security number\t:", make_pair(ssn, "left,setw(8),setfill('#'),showbase"),
+        "Citizen\t\t\t:", make_pair(b, make_tuple(boolalpha)), 
+        "\nheight(meters)\t\t:", make_pair(h, make_tuple(fixed,setprecision(3))),
+        "\nincome\t\t\t:", make_pair(put_money(m),  make_tuple(showbase)),
+        "\nSocial Security number\t:", make_pair(ssn, make_tuple(left,setw(8),setfill('#'),showbase)),
         "\ntime stamp\t\t:", put_time(&tm, "%c %Z"),
         "\n");
- 
 
 }
